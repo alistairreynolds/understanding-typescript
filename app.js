e2035a937c78d2f75e121cdf4f24fcb133ce8233
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 17:34:54 +0000

    Finish lesson 19

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 var person = {
     name: "Alistair",
     age: 34,
-    hobbies: ['cake', "pizza"]
+    hobbies: ['cake', "pizza"],
+    role: [2, 'dev']
 };
+// Throws an error because tuple index 1 = number
+// person.role[1] = 4;
 var hobbies;
 hobbies = ['cake', 'pizza'];
 for (var _i = 0, hobbies_1 = hobbies; _i < hobbies_1.length; _i++) {
--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
-const person = {
+const person: {
+    name: string;
+    age: number
+    hobbies: string[];
+    role: [number, string]; // Denotes a Tuple
+} = {
     name: "Alistair",
     age: 34,
-    hobbies: ['cake', "pizza"]
+    hobbies: ['cake', "pizza"],
+    role: [2, 'dev']
 };
+
+// Throws an error because tuple index 1 = number
+// person.role[1] = 4;
 
 let hobbies: string[];
 hobbies = ['cake', 'pizza'];
 
-for(const hobby of hobbies){
+for (const hobby of hobbies) {
     console.log(hobby.toUpperCase());
 }
