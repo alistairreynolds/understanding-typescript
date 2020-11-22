45b2f18ed8d577e1d4cdc0386ccf45c0920019e8
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 20:30:07 +0000

    Finish lesson 29 on Unknown Types

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 "use strict";
-function add(first, second) {
-    return first + second;
+// Unknown = slightly stricter than "any"
+var userInput;
+var userName;
+userInput = 5;
+// Throws an error because possible mismatch
+// userName = userId;
+if (typeof userInput === 'string') {
+    // Is OK because we're explicitly stating
+    userName = userInput;
 }
-function addAndHandle(a, b, cb) {
-    var r = a + b;
-    cb(r);
-}
-function print(num) {
-    console.log(num);
-}
-// Set the combinedValues var to be a method that requires two number parameters and returns a number
-var combinedValues;
-combinedValues = add;
-print(add(3, 5));
-print(combinedValues(7, 10));
-addAndHandle(3, 5, function (asd) {
-    console.log("Handled results: " + asd);
-});
--- app.ts
+++ app.ts
@@ -1,24 +1,13 @@
-function add(first: number, second: number) {
-    return first + second;
+// Unknown = slightly stricter than "any"
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+
+// Throws an error because possible mismatch
+// userName = userId;
+
+if (typeof userInput === 'string') {
+    // Is OK because we're explicitly stating
+    userName = userInput;
 }
-
-function addAndHandle(a: number, b: number, cb: (a: number) => void) {
-    const r = a + b;
-    cb(r);
-}
-
-function print(num: number): void {
-    console.log(num)
-}
-
-// Set the combinedValues var to be a method that requires two number parameters and returns a number
-let combinedValues: (a: number, b: number) => number;
-
-combinedValues = add;
-
-print(add(3, 5));
-print(combinedValues(7, 10));
-
-addAndHandle(3, 5, (asd) => {
-    console.log(`Handled results: ${asd}`)
-});
