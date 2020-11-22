5dcae338c59425ee1fbc891a2b05ea53c757e8f8
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 19:19:40 +0000

    Finish lesson 23 on Literals

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 "use strict";
-function combine(input1, input2) {
+function combine(input1, input2, resultType // defines a literal
+) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number')) {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
+var combinedAges = combine(20, 50, 'as-number');
+console.log(combinedAges);
+var combinedAgesStrings = combine('20', '50', 'as-number');
+console.log(combinedAgesStrings);
+var combinedNames = combine("Dave", "Suzy", 'as-text');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,9 +1,22 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultType: 'as-number' | 'as-text' // defines a literal
+) {
     let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number')) {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
+
+const combinedAges = combine(20, 50, 'as-number');
+console.log(combinedAges);
+
+const combinedAgesStrings = combine('20', '50', 'as-number');
+console.log(combinedAgesStrings);
+
+const combinedNames = combine("Dave", "Suzy", 'as-text');
+console.log(combinedNames);
