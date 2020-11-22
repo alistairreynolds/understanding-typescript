7ecdb086ec64292902b7f9c05a8e81715fb697c8
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 14:21:24 +0000

    End of lesson 11

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Your code goes heresdsd...');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var first = +"5";
+var second = 7.2;
+var r = add(first, second);
+console.log(r);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Your code goes heresdsd...');
+function add(num1: number, num2: number){
+    return num1 + num2;
+}
+
+const first = +"5";
+const second = 7.2;
+
+const r = add(first, second);
+
+console.log(r);
