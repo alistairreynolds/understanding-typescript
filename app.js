903b4de9c07764fd9f6092a97a661471753f68f4
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 16:21:59 +0000

    End of lesson 14

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 "use strict";
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, showResult, prefix) {
+    var result = prefix + " " + (num1 + num2);
+    if (showResult) {
+        alert(result);
+    }
+    else {
+        return result;
+    }
 }
 var first = +"5";
 var second = 7.2;
-var r = add(first, second);
+var showResult = true;
+var r = add(first, second, showResult, "The result is ");
 console.log(r);
--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
-function add(num1: number, num2: number){
-    return num1 + num2;
+function add(num1: number, num2: number, showResult: boolean, prefix: string){
+    const result =  `${prefix} ${num1 + num2}`;
+    if(showResult){
+        alert(result);
+    }else{
+        return result;
+    }
 }
 
 const first = +"5";
 const second = 7.2;
+const showResult = true;
 
-const r = add(first, second);
+const r = add(first, second, showResult, "The result is ");
 
 console.log(r);
