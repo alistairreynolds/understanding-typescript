f9d4b9e5e60f1c7b99d5e59b451d351798fc5144
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-22 16:40:56 +0000

    End of lesson 16

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 "use strict";
-function add(num1, num2, showResult, prefix) {
-    var result = prefix + " " + (num1 + num2);
-    if (showResult) {
-        alert(result);
-    }
-    else {
-        return result;
-    }
-}
-var first;
-first = 5;
-var second = 7.2;
-var showResult = true;
-var r = add(first, second, showResult, "The result is ");
-console.log(r);
+// Not good practice as inferred
+var person = {
+    name: "Alistair",
+    age: 34
+};
--- app.ts
+++ app.ts
@@ -1,17 +1,5 @@
-function add(num1: number, num2: number, showResult: boolean, prefix: string){
-    const result =  `${prefix} ${num1 + num2}`;
-    if(showResult){
-        alert(result);
-    }else{
-        return result;
-    }
+// Not good practice as inferred
+const person: { name: string; age: number } = {
+    name: "Alistair",
+    age: 34
 }
-
-let first: number;
-first = 5;
-const second = 7.2;
-const showResult = true;
-
-const r = add(first, second, showResult, "The result is ");
-
-console.log(r);
