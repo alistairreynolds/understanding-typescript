2074e644c36bbd538fd928af1bc7e1947e4c6552
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-24 19:50:34 +0000

    Finish lesson 62 - public/private

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log(`Department: ${this.name}`);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeesCount() {
+        console.log(this.employees.length);
+    }
 }
 const accounting = new Department('accounting');
+accounting.addEmployee('dave');
 // Won't work because we've explicitly stated what "this" refers to
 const accountingCopy = { describe: accounting.describe };
 // console.log(accountingCopy.describe());
-// Will work because we've set a name for it
-const accountingOtherCopy = { name: 'cake', describe: accounting.describe };
-console.log(accountingOtherCopy.describe());
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,14 +9,20 @@ class Department {
     describe(this: Department) {
         console.log(`Department: ${this.name}`);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeesCount(){
+        console.log(this.employees.length);
+    }
 }
 
 const accounting = new Department('accounting')
 
+accounting.addEmployee('dave');
+
 // Won't work because we've explicitly stated what "this" refers to
 const accountingCopy = {describe: accounting.describe};
 // console.log(accountingCopy.describe());
-
-// Will work because we've set a name for it
-const accountingOtherCopy = {name: 'cake', describe: accounting.describe};
-console.log(accountingOtherCopy.describe());
