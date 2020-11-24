1d033e57735fa3d05a447a3d1f97b0ed1ee26f76
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-24 19:27:05 +0000

    Finish lesson 59 - classes

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 "use strict";
-// Unknown = slightly stricter than "any"
-var userInput;
-var userName;
-userInput = 5;
-// Throws an error because possible mismatch
-// userName = userId;
-if (typeof userInput === 'string') {
-    // Is OK because we're explicitly stating
-    userName = userInput;
-}
-// Use "never" of it will never return anything, even undefined. ie, if it stops here and doesn't continue
-function throwError(msg, code) {
-    throw ({ message: msg, errorCode: code });
-}
-throwError('asd', 300);
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    return Department;
+}());
+var accounting = new Department('accounting');
+console.log(accounting);
--- app.ts
+++ app.ts
@@ -1,20 +1,9 @@
-// Unknown = slightly stricter than "any"
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-
-// Throws an error because possible mismatch
-// userName = userId;
-
-if (typeof userInput === 'string') {
-    // Is OK because we're explicitly stating
-    userName = userInput;
+class Department{
+    private name: string;
+    constructor(n: string) {
+        this.name = n;
+    }
 }
 
-// Use "never" of it will never return anything, even undefined. ie, if it stops here and doesn't continue
-function throwError(msg: string, code: number): never {
-    throw ({message: msg, errorCode: code})
-}
-
-throwError('asd', 300);
+const accounting = new Department('accounting')
+console.log(accounting);
