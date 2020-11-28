a3aa0a4e1c017b7a1d44d27695330e8fd4bf7e99
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-28 11:03:50 +0000

    Finish lesson 78 - Optional props and functions

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ addT = function (a, b) {
     return a + b;
 };
 var Person = /** @class */ (function () {
-    function Person(age, name) {
+    function Person(age, name, surname) {
         this.age = age;
         this.name = name;
+        this.surname = surname;
     }
     Person.prototype.greet = function (greeting) {
         console.log(greeting + " " + this.name);
     };
+    Person.prototype.farewell = function (greeting) {
+        console.log(greeting + " " + this.name);
+    };
     return Person;
+}());
+var Dog = /** @class */ (function () {
+    function Dog(name, sn) {
+        this.name = name;
+        if (sn) {
+            this.surname = sn;
+        }
+    }
+    return Dog;
 }());
 var user1;
 user1 = new Person(30, 'asdda');
--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@
 // type AddFn = (a: number, b: number) => number;
 
 // Defining as an interface
-interface AddFn{
-    (a: number, b:number): number
+interface AddFn {
+    (a: number, b: number): number
 }
 
 let addT: AddFn;
@@ -13,25 +13,47 @@ addT = (a, b) => {
 }
 
 
-
-interface Named{
+interface Named {
     readonly name: string;
+    readonly surname?: string;
 }
 
-interface Greetable extends Named{
+interface Aged {
+    age: number;
+}
+
+interface Greetable extends Named {
     greet(greeting: string): void;
+
+    farewell?(message: string): void;
 }
 
 
-class Person implements Greetable{
+class Person implements Greetable, Aged {
 
-    constructor(protected age: number, public name: string) {
+    constructor(public age: number, public name: string, public surname?: string) {
     }
 
     greet(greeting: string) {
         console.log(`${greeting} ${this.name}`)
     }
+
+    farewell(greeting: string) {
+        console.log(`${greeting} ${this.name}`)
+    }
 }
+
+class Dog implements Named {
+    surname?: string;
+
+    constructor(public name: string, sn?: string) {
+        if(sn) {
+            this.surname = sn;
+        }
+    }
+
+}
+
 
 let user1: Greetable;
 
