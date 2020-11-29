17a3a497cd9decddef23bbe3b195db9cdcda035b
Alistair Reynolds <alistair@thovex.co.uk>
2020-11-29 11:12:59 +0000

    Finish lesson 90 - Nullish coalescing

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ var person = {
 };
 // Only log job if it exists
 console.log((_a = person.job) === null || _a === void 0 ? void 0 : _a.title);
+var userInput = '';
+// It's empty, so will return default (logical OR)
+console.log(userInput || 'default');
+// It's specifically not null, so will return the user input still (nullish coalescing)
+console.log(userInput !== null && userInput !== void 0 ? userInput : 'default');
--- app.ts
+++ app.ts
@@ -3,6 +3,13 @@ const person = {
     job: {title: 'poop-head', description: 'head of poop'}
 }
 
-
 // Only log job if it exists
 console.log(person.job?.title);
+
+const userInput = '';
+
+// It's empty, so will return default (logical OR)
+console.log(userInput || 'default');
+
+// It's specifically not null, so will return the user input still (nullish coalescing)
+console.log(userInput ?? 'default');
