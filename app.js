7cbaade7150830945a24e39d25ab043b069a6fa9
Alistair Reynolds <alistair@thovex.co.uk>
2020-12-05 11:57:41 +0000

    Finish lesson 99 - generic classes

--- app.js
+++ app.js
@@ -1,48 +1,15 @@
-// This is generic
-var users = [];
-// These are specific
-var users1 = []; // Would be like string[]|number[]
-var users2 = [];
-var users3 = []; // However this does not work
-users1.push('asd');
-users1.push(1);
-users2.push('asd');
-users2.push(1);
-// Would be generic if not for Promise<string>
-var promiseFn = new Promise(function (resolve, reject) {
-    if (Math.random() > 0.5) {
-        resolve('all good');
+var Storage = /** @class */ (function () {
+    function Storage() {
+        this.data = [];
     }
-    else {
-        reject('all bad');
-    }
-});
-promiseFn.then(function (r) { return console.log(r); })["catch"](function (r) { return console.error(r); });
-// Defining a generic function
-// Convention is to use "T", then carry on with the alphabet
-// Using "extends" it constrains the T and U to be objects still
-function merged(objA, objB) {
-    return Object.assign(objA, objB);
-}
-var mergeResult = merged({ name: 'sdada' }, { age: 30 });
-var mergeResult2 = merged({ potatoes: ['red', 'brown'] }, { age: 30 });
-// If we just used (objA: object) on the function definition, it wouldn't be able to know "name" is a property
-console.log(mergeResult.name);
-console.log(mergeResult2.potatoes[0]);
-function countAndDescribe(param) {
-    var description = 'has no length value';
-    if (param.length === 1) {
-        description = "has a a single element ";
-    }
-    else if (param.length > 1) {
-        description = "has " + param.length + " elements";
-    }
-    return [param, description];
-}
-console.log(countAndDescribe(['asdadad', 'dfsf']));
-// keyof
-function extractKey(obj, key) {
-    return obj[key];
-}
-// Will throw an error if there requested key doesn't exist because we're using "keyof" in the generic
-console.log(extractKey({ name: 'dude' }, 'name'));
+    Storage.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    Storage.prototype.removeItem = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    Storage.prototype.getItems = function () {
+        return this.data;
+    };
+    return Storage;
+}());
--- app.ts
+++ app.ts
@@ -1,71 +1,33 @@
-// This is generic
-const users: [] = []
+class DataStorage<T extends String | Number | Boolean>{
+    // Define a custom/generic type so that it doesn't really matter what gets passed
+    // Don't allow objects as they won't be removed very well
+    private data : T[] = [];
 
-// These are specific
-const users1: Array<string | number> = [];     // Would be like string[]|number[]
-const users2: (string | number)[] = [];
-const users3: string[] | number[] = [];       // However this does not work
-
-
-users1.push('asd');
-users1.push(1);
-users2.push('asd');
-users2.push(1);
-
-
-// Would be generic if not for Promise<string>
-const promiseFn: Promise<string> = new Promise((resolve, reject) => {
-    if (Math.random() > 0.5) {
-        resolve('all good');
-    } else {
-        reject('all bad');
-    }
-})
-
-promiseFn.then(r => console.log(r)).catch(r => console.error(r));
-
-
-// Defining a generic function
-// Convention is to use "T", then carry on with the alphabet
-// Using "extends" it constrains the T and U to be objects still
-function merged<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
-}
-
-const mergeResult = merged({name: 'sdada'}, {age: 30});
-const mergeResult2 = merged({potatoes: ['red', 'brown']}, {age: 30});
-
-// If we just used (objA: object) on the function definition, it wouldn't be able to know "name" is a property
-console.log(mergeResult.name);
-console.log(mergeResult2.potatoes[0]);
-
-
-// Won't work as it's a constrained generic
-// const constrainedMergeResult = merged({name: 'sdada'},  30);
-
-interface HasLength {
-    // State that it needs to have a length property
-    length: number;
-}
-
-function countAndDescribe<T extends HasLength>(param: T): [T, String] {
-    let description = 'has no length value';
-    if (param.length === 1) {
-        description = `has a a single element `;
-    } else if (param.length > 1) {
-        description = `has ${param.length} elements`;
+    addItem(item: T){
+        this.data.push(item);
     }
 
-    return [param, description];
+    removeItem(item: T){
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems(){
+        return this.data;
+    }
 }
 
-console.log(countAndDescribe(['asdadad', 'dfsf']));
 
-// keyof
+// Now we define what we want to store when we instantiate the object
+const textStorage = new DataStorage<String>()
+textStorage.addItem('cake')
 
-function extractKey<T extends Object, U extends keyof T>(obj: T, key: U){
-    return obj[key];
-}
+const numberStorage = new DataStorage<Number>()
+numberStorage.addItem(10)
 
-// Will throw an error if there requested key doesn't exist because we're using "keyof" in the generic
-console.log(extractKey({name: 'dude'}, 'name'));
+
+// This won't work because we're creating the new object when we trying to remove it.
+// Only way around would be to create the object and reference it when removing, which defeats the point
+
+// const objStorage = new DataStorage<Object>()
+// numberStorage.addItem({name: 'bill'})
+// numberStorage.removeItem({name: 'bill'})
