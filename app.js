19a1bf744aac8a0f700e6cf5559471a75f0ec907
Alistair Reynolds <alistair@thovex.co.uk>
2020-12-05 14:02:47 +0000

    Finish lesson 105 - first decorator

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-var DataStorage = /** @class */ (function () {
-    function DataStorage() {
-        // Define a custom/generic type so that it doesn't really matter what gets passed
-        // Don't allow objects as they won't be removed very well
-        this.data = [];
-    }
-    DataStorage.prototype.addItem = function (item) {
-        this.data.push(item);
-    };
-    DataStorage.prototype.removeItem = function (item) {
-        this.data.splice(this.data.indexOf(item), 1);
-    };
-    DataStorage.prototype.getItems = function () {
-        return this.data;
-    };
-    return DataStorage;
-}());
-// Now we define what we want to store when we instantiate the object
-var textStorage = new DataStorage();
-textStorage.addItem('cake');
-var numberStorage = new DataStorage();
-numberStorage.addItem(10);
-function createCourseGoal(title, description, completeBy) {
-    // Partial allows you to turn every parameter as optional
-    var obj = {};
-    obj.title = title;
-    obj.description = description;
-    obj.completeBy = completeBy;
-    // Type cast so that it returns as a CourseGoal properly
-    return obj;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// Decorator is just a function, usually Pascal case
+function Logger(constructor) {
+    console.log('Logging');
+    console.log(constructor);
 }
-console.log(createCourseGoal('sad', 'fsdfs', new Date()));
-var names = ['dave', 'bill'];
-// Can no longer add to names
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = 'me';
+        console.log('Creating person');
+    }
+    Person = __decorate([
+        Logger // Says you want to use a decorator here
+    ], Person);
+    return Person;
+}());
+var person = new Person();
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,60 +1,20 @@
-class DataStorage<T extends String | Number | Boolean> {
-    // Define a custom/generic type so that it doesn't really matter what gets passed
-    // Don't allow objects as they won't be removed very well
-    private data: T[] = [];
+// Decorator is just a function, usually Pascal case
+function Logger(constructor: Function){
+    console.log('Logging');
+    console.log(constructor);
+}
 
-    addItem(item: T) {
-        this.data.push(item);
+@Logger // Says you want to use a decorator here
+class Person{
+    name = 'me';
+
+    constructor() {
+        console.log('Creating person');
     }
 
-    removeItem(item: T) {
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-
-    getItems() {
-        return this.data;
-    }
 }
 
 
-// Now we define what we want to store when we instantiate the object
-const textStorage = new DataStorage<String>()
-textStorage.addItem('cake')
+const person = new Person();
 
-const numberStorage = new DataStorage<Number>()
-numberStorage.addItem(10)
-
-
-// This won't work because we're creating the new object when we trying to remove it.
-// Only way around would be to create the object and reference it when removing, which defeats the point
-
-// const objStorage = new DataStorage<Object>()
-// numberStorage.addItem({name: 'bill'})
-// numberStorage.removeItem({name: 'bill'})
-
-
-interface CourseGoal {
-    title: string,
-    description: string,
-    completeBy: Date,
-}
-
-function createCourseGoal(title: string, description: string, completeBy: Date): CourseGoal {
-
-    // Partial allows you to turn every parameter as optional
-    let obj: Partial<CourseGoal> = {};
-
-    obj.title = title;
-    obj.description = description;
-    obj.completeBy = completeBy;
-
-    // Type cast so that it returns as a CourseGoal properly
-    return obj as CourseGoal;
-}
-
-
-console.log(createCourseGoal('sad', 'fsdfs', new Date()))
-
-
-const names: Readonly<string[]> = ['dave', 'bill'];
-// Can no longer add to names
+console.log(person);
